048bd652b52ee1375c354e9b5d78828040a73b74
Norine NDOUDI <norinendoudi@gmail.com>
2022-10-04 18:07:14 +0200

    feat: Partie 4 et 5 du lab 2

--- handles.js
+++ handles.js
@@ -6,20 +6,73 @@ const url = require('url')
 const qs = require('querystring')
 
 module.exports = {
-    
-    serverHandle: function (req, res) {
-      const route = url.parse(req.url)
-      const path = route.pathname 
-      const params = qs.parse(route.query)
-    
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-    
-      if (path === '/hello' && 'name' in params) {
-        res.write('Hello ' + params['name'])
-      } else {
-        res.write('Hello anonymous')
+
+  serverHandle: function (req, res) {
+    const route = url.parse(req.url)
+    const path = route.pathname
+    const params = qs.parse(route.query)
+
+    if (path === '/hello' && 'name' in params) {
+
+      if (params['name'] == "Norine") {
+       const content = '<!DOCTYPE html>' +
+          '<html>' +
+          '    <head>' +
+          '        <meta charset="utf-8" />' +
+          '        <title>Hello Norine</title>' +
+          '    </head>' +
+          '    <body>' +
+          '       <h1>Coucou</h1>' +
+          '       <p>Hello je m\'appelle Norine j\'ai 19 ans et je suis en ING4 à l\'ECE Paris !</p>' +
+          '    </body>' +
+          '</html>'
+        res.writeHead('400', { 'Content-Type': 'text/html' });
+        res.write(content);
       }
-      
-      res.end();
-    } 
+      else {
+        res.write('Hello ' + params['name'])
+      }
+    }
+    else if(path==='/')
+    {
+      const content = '<!DOCTYPE html>' +
+          '<html>' +
+          '    <head>' +
+          '        <meta charset="utf-8" />' +
+          '        <title>Utilisation du hello</title>' +
+          '    </head>' +
+          '    <body>' +
+          '       <h1 style="text-align:center;">Utilisation du hello</h1>' +
+          '       <p>Nous avons l\'adresse URL suivante: http://localhost:8080/ nous allons parler du fonctionnement de la page hello dans l\'URL</p>' +
+          '       <p>Il suffit d\'entrer hello et votre nom de la manière suivant: <b> http://localhost:8080/hello?name=votre_nom </b> </p>' +
+          '       <p>Par exemple, quelqu\'un qui s\'appelle John écrira <b><a href ="http://localhost:8080/hello?name=John"> http://localhost:8080/hello?name=John</a></b></p>' +
+          '       <p> Si vous taper le nom de la créatrice du site Norine vous obtiendez une petite description de sa part <br>' +
+          '        <b><a href="http://localhost:8080/hello?name=Norine"> http://localhost:8080/hello?name=Norine </a> </b> </p>' +
+          '        <br>' +
+          '        <b><p> ATTENTION SI VOUS NE TAPEZ PAS DE PARAMETRE NAME, VOUS TOMBEREZ SUR UNE PAGE ERREUR </p></b>' +
+          '    </body>' +
+          '</html>'
+        res.writeHead('400', { 'Content-Type': 'text/html' });        
+        res.write(content);
+    }
+    else
+    {
+        const content = '<!DOCTYPE html>' +
+          '<html>' +
+          '    <head>' +
+          '        <meta charset="utf-8" />' +
+          '        <title>BIP BIP BIP</title>' +
+          '    </head>' +
+          '    <body>' +
+          '       <h1>ERROR 404 <h1>' +
+          '       <h5>Page introuvable </h5>' +
+          '    </body>' +
+          '</html>'
+        
+          res.writeHead('400', { 'Content-Type': 'text/html' });
+        res.write(content);
+    }
+
+    res.end();
   }
+}
--- index.js
+++ index.js
@@ -5,14 +5,5 @@ const server = http.createServer(handles.serverHandle);
 server.listen(8080)
 
 // Define a string constant concatenating strings
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
+
 
