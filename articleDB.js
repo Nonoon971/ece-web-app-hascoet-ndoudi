84e012b5c081c3a72a2e0dc0f74ee3c1c6f2458f
Norine NDOUDI <norinendoudi@gmail.com>
2022-10-12 00:25:16 +0200

    fix: gitignore

---
+++ articleDB.js
@@ -0,0 +1,35 @@
+const { application } = require("express")
+
+let db = {
+    articles: [
+      {
+        id: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        title: 'My article',
+        content: 'Content of the article.',
+        date: '04/10/2022',
+        author: 'Liz Gringer'
+      },
+      // ...
+    ],
+    comments: [
+      {
+        id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+        timestamp: 1664835049,
+        content: 'Content of the comment.',
+        articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        author: 'Bob McLaren'
+      },
+      // ...
+    ]
+  }
+
+app.get('/articles',(req,res) => {
+    for(const element of db.articles)
+    {
+        res.send(element)
+    }
+})
+
+app.post('/articles',(req,res) => {
+
+})
