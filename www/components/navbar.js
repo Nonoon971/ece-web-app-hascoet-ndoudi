194bd9254c5f766f83aa7e84b1cdf20aefa0b939
Norine NDOUDI <norinendoudi@gmail.com>
2022-11-09 15:45:32 +0100

    feat:ajout du bouton de navigation accueil et de connextion

--- www/components/navbar.js
+++ www/components/navbar.js
@@ -1,7 +1,7 @@
 export const  Navbar = () => {
   return (
     <nav className='bg-blue-500 text-center p-5 w-full text-lg mb-5'>
-
+      
           <a href="/" className="inline-flex shadow-lg float-left lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-red-800 hover:text-white">Accueil</a>
           <a href="/about" className="inline-flex shadow-lg lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-800 hover:text-white">About</a>
 
