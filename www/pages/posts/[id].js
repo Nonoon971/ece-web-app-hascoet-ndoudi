d1ec0b513fe4ea4b835a95b2e8ab33b003aa2bb3
Norine <norinendoudi@gmail.com>
2022-12-20 19:31:51 +0100

    feat: affichage article selectionne via supabase

--- www/pages/posts/[id].js
+++ www/pages/posts/[id].js
@@ -1,23 +1,44 @@
-import db from "../../content/db";
-import Layout from "../../components/layout";
-import Link from 'next/link';
+import { useState, useEffect } from 'react'
+import Layout from '../../components/layout'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
 
-export default function Post(props) {
-
-  const article = props.post;
+//export default function Post(props) {
+export default function Article({id}) {
+  const [article, setArticles] = useState(null)
+  const supabase = useSupabaseClient()
+  useEffect(() => {
+    (async () => {
+      let { data, error, status } = await supabase
+        .from('articles')
+        .select()
+        .eq('id', id)
+        setArticles(data)
+        console.log(data)
+    })()
+  }, [id])
+  console.log(article)
+  //const article = props.post;
+  let lien = "https://xxpeqblsyczvsphynbzo.supabase.co/storage/v1/object/public/joueurs/"
 
   return (
     <div>
       <Layout>
         <div className="text-center bg-sky-300 p-3 mt-3 mx-auto rounded-md mb-6 max-w-xl">
-          <h1>Détails de l'article {article.id}</h1>
+          <h1>Détails du joueur {id}</h1>
           <p className="italic font-bold hover:text-red-800">
-            Titre: {article.title}
+            Nom: {article[0].title}
+            <img
+              src={lien + article[0].image}
+              alt="Picture of the player"
+              width={(200)}
+              height={200}
+            />
             <br />
-            Content: {article.date}
+            Date début de carrière: {article[0].date}
             <br />
-            Auteur: {article.author}
+            Son équipe: {article[0].team}
             <br />
+
           </p><br/>
           <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
               Supprimer cette article
@@ -27,7 +48,14 @@ export default function Post(props) {
     </div>
   );
 }
-
+export async function getServerSideProps(context) {
+    return {
+      props: {
+        id: context.params.id
+      },
+    }
+  }
+/*
 export async function getStaticPaths() {
   return {
     paths: db.posts.map((post) => ({ params: { id: post.id } })),
@@ -42,3 +70,4 @@ export async function getStaticProps(context) {
     props: { post: db.posts.filter((post) => post.id == context.params.id)[0] },
   };
 }
+*/
