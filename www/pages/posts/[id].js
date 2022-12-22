b8b03211b45f07b0771ba944475034e410b52ef0
Norine <norinendoudi@gmail.com>
2022-12-22 23:01:46 +0100

    feat: modification/reponse/suppression commentaires

--- www/pages/posts/[id].js
+++ www/pages/posts/[id].js
@@ -26,14 +26,18 @@ export default function Article({ id }) {
     })()
   }, [id])
 
+  console.log(article)
   //Bouton suppression si c'est l'auteur de l'article
   if (article && user) {
+    let modification = document.getElementsByClassName('modification')
     let createur = document.getElementById("createur")
     if (article[0].user_id == user.id) {
       createur.innerHTML = "Supprimer l'article"
       createur.className = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded float-right'
     }
   }
+
+
 
   //Fonction de suppression
   const suppression = async function (e) {
@@ -82,7 +86,7 @@ export default function Article({ id }) {
     }
     else //Sinon on insère sans et ça aura la valeur null
     {
-      pseudo = "anonyme"
+      let pseudo = "anonyme"
       const { data3, error3 } = await supabase
         .from('comments')
         .insert({ title, content, pseudo, article_id })
@@ -107,6 +111,37 @@ export default function Article({ id }) {
     click = click + 1;
     let reponse = document.getElementById('newreponse')
     let span = document.getElementById('idcomments')
+    if (click % 2 == 0) {
+      reponse.className = "max-w-lg rounded-lg shadow-md shadow-blue-600/50 m-auto border-black border-2 hidden"
+    }
+    else {
+      reponse.className = "max-w-lg rounded-lg shadow-md shadow-blue-600/50 mx-auto border-black border-2"
+      span.innerHTML = comments_id
+    }
+    console.log(comments_id)
+  }
+  function modify() {
+    console.log(comments_id)
+    console.log(click)
+    click = click + 1;
+    let reponse = document.getElementById('modification')
+    let span = document.getElementById('idmodif')
+    if (click % 2 == 0) {
+      reponse.className = "max-w-lg rounded-lg shadow-md shadow-blue-600/50 m-auto border-black border-2 hidden"
+    }
+    else {
+      reponse.className = "max-w-lg rounded-lg shadow-md shadow-blue-600/50 mx-auto border-black border-2"
+      span.innerHTML = comments_id
+    }
+    console.log(comments_id)
+  }
+
+  function suppwindow() {
+    console.log(comments_id)
+    console.log(click)
+    click = click + 1;
+    let reponse = document.getElementById('suppwindow')
+    let span = document.getElementById('idsupp')
     if (click % 2 == 0) {
       reponse.className = "max-w-lg rounded-lg shadow-md shadow-blue-600/50 m-auto border-black border-2 hidden"
     }
@@ -142,7 +177,7 @@ export default function Article({ id }) {
         console.log(pseudo)
         console.log(user_id)
         console.log(comments_id)
-        //router.reload(window.location.pathname)
+        router.reload(window.location.pathname)
 
       }
     }
@@ -165,6 +200,50 @@ export default function Article({ id }) {
     }
   }
 
+  //Fonction modifer les commentaires
+  const modifcomments = async function (e) {
+    e.preventDefault()
+    // Si authentifié on insère avec l'user_id
+    if (user) {
+      const {error7 } = await supabase
+        .from('comments')
+        .update({content, title})
+        .eq('id',comments_id)
+
+      // Print a friendly confirmation message
+      if (error7) {
+        console.log("error")
+        console.log(error7)
+      }
+      else {
+        console.log("modification commentaire")
+        router.reload(window.location.pathname)
+
+      }
+    }
+  }
+  //Fonction supprimer les commentaires
+  const suppcomment = async function (e) {
+    e.preventDefault()
+    // Si authentifié on insère avec l'user_id
+    if (comments_id != undefined) {
+      const {error8} = await supabase
+        .from('comments')
+        .delete()
+        .eq('id',comments_id)
+
+      // Print a friendly confirmation message
+      if (error8) {
+        console.log("error")
+        console.log(error8)
+      }
+      else {
+        console.log("suppression commentaire")
+        router.reload(window.location.pathname)
+
+      }
+    }
+  }
   let lien = "https://xxpeqblsyczvsphynbzo.supabase.co/storage/v1/object/public/joueurs/"
 
   return (
@@ -223,21 +302,21 @@ export default function Article({ id }) {
         </div>
 
         {article && (
-          <div className="antialiased mx-auto max-w-screen-sm float-right mb-24 mr-5">
+          <div className="antialiased mx-auto max-w-screen-sm float-right mb-24 mr-5 w-1/2">
             <h3 className="mb-4 text-lg font-semibold text-gray-900">Comments</h3>
 
             <div className="space-y-4">
 
-             <div id='newreponse' className="max-w-lg rounded-lg shadow-md shadow-blue-600/50 m-auto border-black border-2 hidden">
+            <div id='newreponse' className="max-w-lg rounded-lg shadow-md shadow-blue-600/50 m-auto border-black border-2 hidden">
             <form onSubmit={sscomment} className="w-full p-4">
               <div className="mb-2">
-                <label className="text-lg font-bold underline">Add an answer to the comment n° <span id='idcomments'></span></label><br/>
+                <label className="text-lg font-bold underline">Add an answer to the comment n° <span id='idcomments' className=' text-orange-500'></span></label><br/>
                 <p className=' text-xs text-gray-500'>Click again on reply in order to close this tab</p>
                 <div>
                   <label className=' font-bold float-left'>Votre commentaire<span className='text-red-700'>*</span>: </label><br />
                   <textarea
                     className="w-full h-20 p-2 border rounded focus:outline-none focus:ring-gray-300 focus:ring-1"
-                    name="comment" value={contenu} onChange={(e) => setContenu(e.target.value)} required
+                     value={contenu} onChange={(e) => setContenu(e.target.value)} required
                     placeholder="">
                   </textarea>
                 </div>
@@ -250,6 +329,46 @@ export default function Article({ id }) {
             </form>
           </div>
 
+          <div id='modification' className="max-w-lg rounded-lg shadow-md shadow-blue-600/50 m-auto border-black border-2 hidden">
+            <form onSubmit={modifcomments} className="w-full p-4">
+              <div className="mb-2">
+                <label className="text-lg font-bold underline">Modify your comment n° <span id='idmodif' className=' text-orange-500'></span></label><br/>
+                <p className=' text-xs text-gray-500'>Click again on modify in order to close this tab</p>
+                <div>
+                <div>
+                  <label className=' font-bold float-left'>Titre: </label><br />
+                  <input type="text" className='rounded-lg border block p-2' value={title} onChange={(e) => setTitle(e.target.value)}  ></input>
+                </div><br />
+                  <label className=' font-bold float-left'>Votre commentaire<span className='text-red-700'>*</span>: </label><br />
+                  <textarea
+                    className="w-full h-20 p-2 border rounded focus:outline-none focus:ring-gray-300 focus:ring-1"
+                   value={content} onChange={(e) => setContent(e.target.value)} required
+                    placeholder="">
+                  </textarea>
+                </div>
+              </div>
+              <div>
+                <button type='submit' className="px-3 py-2 text-sm text-blue-100 bg-blue-600 rounded">
+                  Modify
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div id='suppwindow' className="max-w-lg rounded-lg shadow-md shadow-blue-600/50 m-auto border-black border-2 hidden">
+            <form onSubmit={suppcomment} className="w-full p-4">
+              <div className="mb-2">
+                <label className="text-lg font-bold underline">Etes vous sûr de vouloir supprimer le commentaire n°<span id='idsupp' className=' text-orange-500'></span></label><br/>
+                <p className=' text-xs text-gray-500'>Click again on suppression in order to close this tab</p>
+                <p className=' text-xs text-gray-500'>
+                  <button type='submit' className="px-3 py-2 text-sm text-blue-100 bg-blue-600 rounded">
+                    Oui
+                  </button>
+                </p>
+              </div>
+            </form>
+          </div>
+
               {article[0].comments.map(comments => (
                 <div className="flex">
                   <div className="flex-shrink-0 mr-3">
@@ -258,14 +377,18 @@ export default function Article({ id }) {
                   <div className="flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
                     <strong>{comments.pseudo}</strong> <span className="text-xs text-gray-400">{comments.created_at}</span>
                     <span className='text-sm ml-36 font-bold text-gray-500 float-right'>N° {comments.id}</span>
-                    <span className=' text-lg ml-36 font-bold text-red-800 float-right'>Titre: {comments.title}</span>
+                    <span className=' text-sm ml-36 font-bold text-red-800 float-right'>Titre: {comments.title}</span>
                     <p className="text-sm">
                       {comments.content}
                     </p>
+                    {user && (comments.user_id==user.id) &&
+                        <div>
+                        <button className=" float-right my-5 uppercase tracking-wide text-red-600 font-bold text-xs ml-3" onClick={() => {setCommentsID(comments.id); console.log(comments_id); suppwindow()}} > Suppression</button>
+                        <button className="float-right my-5 uppercase tracking-wide text-blue-600 font-bold text-xs ml-3" onClick={() => {setCommentsID(comments.id); modify(); }} > Modify</button>
 
+                        </div>
+                    }
                       <button onClick={() => {setCommentsID(comments.id); response(); }} className="my-5 uppercase tracking-wide text-gray-400 font-bold text-xs"> + Replies</button>
-                      <button value={comments.id} className="my-5 uppercase tracking-wide text-gray-400 font-bold text-xs ml-3"> Modify </button>
-
                       <div className="space-y-4">
                     {comments.answercomments.map(answers => (
 
